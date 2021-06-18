6f8e7a6ee32073047aad3ef0a62516eb7639fab6
Vitor Cordeiro Paes Prieto <67298769+SonngCS@users.noreply.github.com>
2021-06-18 11:42:31 -0300

    Add files via upload

---
+++ OBI/SENIOR LEVEL/zero.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(void){
+    int n, i, somatorio, j;
+    scanf("%d", &n);
+
+    int soma[n];
+
+    for (i = 0; i < n; i++){
+        scanf("%d", &soma[i]);
+    }
+
+    for(i = 0; i < n; i++){
+        somatorio += soma[i];
+    }
+
+    for(i = 0; i < n; i++){
+      if(soma[i] == 0 && soma[i-1] == 0){
+          j = i;
+          while(soma[j] == 0){
+              j--;
+          }
+        somatorio -= soma[j];
+        soma[j] = 0;
+      } else{
+          if(soma[i] == 0){
+              somatorio -= soma[i-1];
+              soma[i-1] = 0;
+          }
+      }
+    }
+
+    printf("%d\n", somatorio);
+
+    return 0;
+}
