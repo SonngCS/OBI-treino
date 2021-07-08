56f07c805631947d20edf41bba4601b10a7795fb
Vitor Cordeiro Paes Prieto <67298769+SonngCS@users.noreply.github.com>
2021-07-08 11:10:08 -0300

    Update pontuacao.c

--- OBI/LEVEL 2/pontuacao.c
+++ OBI/LEVEL 2/pontuacao.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main(){
-    int n, p, p1 = 0, p2 = 0, counter = 0;
+    int n, p, p1, p2, counter;
+    p1 = p2 = counter = 0;
     scanf("%d%d", &n, &p);
     for(int i = 0; i < n; i++){
         scanf(" %d%d", &p1, &p2);
