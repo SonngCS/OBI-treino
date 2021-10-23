22bd3cf70c21fe3d41918dfdb85d8fb0e19b0ed6
Vitor Cordeiro Paes Prieto <67298769+SonngCS@users.noreply.github.com>
2021-10-22 22:17:30 -0300

    Update corrida.c

--- OBI/LEVEL 1/corrida.c
+++ OBI/LEVEL 1/corrida.c
@@ -1,12 +1,14 @@
+//Source problem: https://olimpiada.ic.unicamp.br/pratique/p1/2012/f1/corrida/
+
 #include <stdio.h>
 
 int main(void){
     int c, n, termino;
 
-    scanf("%i %i", &c, &n);
+    scanf("%d %d", &c, &n);
 
     termino = c % n;
-    printf("%i", termino);
+    printf("%d\n", termino);
     return 0;
 
 }
